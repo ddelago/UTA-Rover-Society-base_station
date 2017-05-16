f597f6d15e7f9596d38505c1776e345ea329c6cb
rghamilton3 <rghamilton3@gmail.com>
2017-05-16 15:26:21 -0500

    Removed old files

--- catkin_ws/devel/share/object_msgs/cmake/object_msgs-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(object_msgs_MESSAGE_FILES "/home/rover/rover_embedded/catkin_ws/src/object_msgs/msg/Object.msg;/home/rover/rover_embedded/catkin_ws/src/object_msgs/msg/ObjectPose.msg")
-set(object_msgs_SERVICE_FILES "/home/rover/rover_embedded/catkin_ws/src/object_msgs/srv/ObjectInfo.srv;/home/rover/rover_embedded/catkin_ws/src/object_msgs/srv/RegisterObject.srv")
--- catkin_ws/install/share/convenience_ros_functions/cmake/export_definitions.cmake
+++
@@ -1,2 +0,0 @@
-# This file can be used as catkin CFG_EXTRAS to export baselib_binding compiler definitions 
-set(convenience_ros_functions_DEFINITIONS ${baselib_binding_DEFINITIONS})
